004e34e15ee37bd71bcbbe9b03fe498f3053acb8
hainam682003 <hocon06082003@gmail.com>
2025-05-06 22:22:12 +0700

    Update Linh.js

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 const CONFIG = {
-    titleWeb: "Anh Chỉ Muốn Nói Là",
-    introTitle: ' Trần Quỳnh Như💗',
-    introDesc: `Em yêu anh nồng nàn và thơm tho.Đậm đặc như Comfort một lần xả.`,
+    titleWeb: "Mình Chỉ Muốn Nói Là",
+    introTitle: ' Nguyễn Khánh Linh à  ',
+    introDesc: `Nếu Bạn Kêu chán thì đã có mình ngay đây `,
     btnIntro: '^^HiHi^^',
     title: 'Ánh nắng kia chỉ muốn vươn tay tới lá 🥰',
-    desc: 'Còn Anh thì chỉ muốn hôn má Em thôi 💕💕',
+    desc: 'Còn mình thì chỉ muốn hôn má Linh thôi 💕💕',
     btnYes: 'OKiiiii nhoa <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao mình lại thấy bạn dễ thương  <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: ' 🥰.Anh Yêu Em nhiều nhiều 😘😘',
-    messDesc: 'Em Đồng Ý Làm Người Yêu Anh Nha💗💕.',
+    reply: 'Nếu Có gì không vui thì hãy nói với mình <33333333',
+    mess: ' 🥰Nhiệm vụ mới của mình là lm bn vui 😘😘',
+    messDesc: 'Nếu bạn chưa có người yêu thì cho mìn lm quen nha
+    💗💕.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/tranhai.nam.682003' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
